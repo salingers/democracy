f095162c0ee63f936b697f533992f5479914c4dc
salingers <salingersms@msn.com>
2016-12-20 23:07:56 +0800

    democracy

--- contract/Democracy.sol
+++ contract/Democracy.sol
@@ -2,16 +2,19 @@ pragma solidity ^0.4.2;
 contract owned {
     address public owner;
 
-    function owned() {
+    function owned() 
+	{
         owner = msg.sender;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner 
+	{
         if (msg.sender != owner) throw;
         _;
     }
 
-    function transferOwnership(address newOwner) onlyOwner {
+    function transferOwnership(address newOwner) onlyOwner 
+	{
         owner = newOwner;
     }
 }
